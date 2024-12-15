c80765f029453155442632731c8d2644229cf165
seiji sugiyama <seiji_sugiyama@risebranding.jp>
2024-12-15 17:49:42 +0900

    Update next.config.js
    ; つけた

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ const nextConfig = {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
       },
-}
+};
 
 module.exports = nextConfig
