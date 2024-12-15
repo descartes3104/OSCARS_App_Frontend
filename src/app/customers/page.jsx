00db31dd9dad457eefa7b32cce1326b81dc44f19
seiji sugiyama <seiji_sugiyama@risebranding.jp>
2024-12-15 13:50:09 +0900

    Update page.jsx
    update/page.jsxのエラーハンドリングを強化

--- src/app/customers/page.jsx
+++ src/app/customers/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import OneCustomerInfoCard from "src/app/components/one_customer_info_card.jsx";
-import Link from 'next/link'
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import fetchCustomers from "./fetchCustomers";
 
@@ -8,11 +8,11 @@ export default function Page() {
   const [customerInfos, setCustomerInfos] = useState([]);
 
   useEffect(() => {
-      const fetchAndSetCustomer = async () => {
-          const customerData = await fetchCustomers();
-          setCustomerInfos(customerData);
-      };
-      fetchAndSetCustomer();
+    const fetchAndSetCustomer = async () => {
+      const customerData = await fetchCustomers();
+      setCustomerInfos(customerData);
+    };
+    fetchAndSetCustomer();
   }, []);
 
   return (
@@ -23,23 +23,28 @@ export default function Page() {
         </Link>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-
-        {customerInfos.map((customerInfo, index) => (
-          <div key={index} className="card bordered bg-white border-blue-200 border-2 flex flex-row max-w-sm m-4">
-            <OneCustomerInfoCard {...customerInfo} />
-            <div className="card-body flex flex-col justify-between">
-              <Link href={`/customers/read/${customerInfo.customer_id}`}>
-                <button className="btn btn-neutral w-20 border-0 bg-blue-200 text-black hover:text-white">Read</button>
-              </Link>
-              <Link href={`/customers/update/${customerInfo.customer_id}`}>
-                <button className="btn btn-neutral w-20 border-0 bg-blue-200 text-black hover:text-white">Update</button>
-              </Link>
-              <Link href={`/customers/delete/${customerInfo.customer_id}`}>
-                <button className="btn btn-neutral w-20 border-0 bg-blue-200 text-black hover:text-white">Delete</button>
-              </Link>
+        {customerInfos && customerInfos.length > 0 ? (
+          customerInfos.map((customerInfo, index) => (
+            <div key={index} className="card bordered bg-white border-blue-200 border-2 flex flex-row max-w-sm m-4">
+              <OneCustomerInfoCard {...customerInfo} />
+              <div className="card-body flex flex-col justify-between">
+                <Link href={`/customers/read/${customerInfo.customer_id}`}>
+                  <button className="btn btn-neutral w-20 border-0 bg-blue-200 text-black hover:text-white">Read</button>
+                </Link>
+                <Link href={`/customers/update/${customerInfo.customer_id}`}>
+                  <button className="btn btn-neutral w-20 border-0 bg-blue-200 text-black hover:text-white">Update</button>
+                </Link>
+                <Link href={`/customers/delete/${customerInfo.customer_id}`}>
+                  <button className="btn btn-neutral w-20 border-0 bg-blue-200 text-black hover:text-white">Delete</button>
+                </Link>
+              </div>
             </div>
+          ))
+        ) : (
+          <div className="text-center text-gray-500 mt-10">
+            <p>No customers found.</p>
           </div>
-        ))}
+        )}
       </div>
     </>
   );
